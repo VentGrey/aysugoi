9494c21fe353b9c562bac0ffd854c0652b8cbf9d
VentGrey <omarpurataf@gmail.com>
2023-03-16 23:00:31 -0600

    Add username changes and save to os file

--- settings_button.go
+++ settings_button.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
@@ -10,16 +14,38 @@ import (
 )
 
 func NewSettingsButton(window fyne.Window) *widget.Button {
+	user_home, err := os.UserHomeDir()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	settingsFile := filepath.Join(user_home, ".aysugoi_config")
+
+	usernameData, err := ioutil.ReadFile(settingsFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	username := strings.TrimSpace(string(usernameData))
+
 	settingsButton := widget.NewButtonWithIcon("", theme.SettingsIcon(), func() {
 		usernameEntry := widget.NewEntry()
-		messageLabel := widget.NewLabel("Sync with friends coming soon!")
+		usernameEntry.SetText(username)
+		messageLabel := widget.NewLabel("Sync with friends coming soon!\n\nUsername: " + username + "\n\nFor your username change to take effect, you must restart the app.")
 
 		dialog.NewForm("Settings", "Save", "Cancel", []*widget.FormItem{
 			widget.NewFormItem("Username", usernameEntry),
 			widget.NewFormItem("", messageLabel),
 		}, func(save bool) {
 			if save {
-				fmt.Println("Username:", usernameEntry.Text)
+				err := ioutil.WriteFile(settingsFile, []byte(usernameEntry.Text), 0644)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
 			}
 		}, window).Show()
 	})
